ad204ca5fe32dd63a52186e2f02df5b8152d9895
wsyxbcl <wsyxbcl@gmail.com>
2022-06-04 01:38:03 +0800

    add checkin operation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use clap::Parser;
 use teloxide::prelude2::*;
 use xmu_health_report_rust_sdk::{
-    create_client, get_system_date, is_today_reported, login, get_continuous_report_day_count,
+    create_client, is_today_reported, login, get_continuous_report_day_count, report, ReportStage,
   };
 
+use std::collections::HashMap;
 
 /// send xmu health checkin status to telegram user 
 #[derive(Parser, Debug)]
@@ -27,18 +28,30 @@ async fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
 
     // bot initiate
-    teloxide::enable_logging!();
-    log::info!("Starting simple_commands_bot...");
     let bot = Bot::from_env().auto_send();
 
     let username = args.username;
     let password = args.password;
     let client = create_client().await?;
     login(&client, username.as_str(), password.as_str()).await?;
-    let (today, form_date) = is_today_reported(&client).await?;
+
+    let report_result = report(&client, &HashMap::new()).await?;
+
+    bot.send_message(args.chat_id, 
+        format!(
+                "Checking in... : {}",
+                if report_result.status_code == ReportStage::ReportSuccess {
+                "CHECKED"
+                } else {
+                "FAILED"
+                }
+            )
+        ).await?;
+
+    let (today, _) = is_today_reported(&client).await?;
     let days_count = get_continuous_report_day_count(&client).await?;
     bot.send_message(args.chat_id, 
-                format!("Local date: {}\n Server date: {}\n Status: {}, checked for {} days", 
-                        get_system_date(), form_date, if today { "check" } else { "uncheck" }, days_count)).await?;
+                format!("Status: {}, checked for {} days", 
+                        if today { "checked" } else { "unchecked" }, days_count)).await?;
     Ok(())
 }
